95446573ec7ca19228cfd46541e8db5670117401
Henry Robert Muwanika <hrmuwanika@gmail.com>
2019-02-24 21:01:05 +0200

    Update objectdetection.py

--- objectdetection.py
+++ objectdetection.py
@@ -1,14 +1,14 @@
 from imageai.Detection import ObjectDetection
 import os
+
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "download.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image2.jpg"), output_image_path=os.path.join(execution_path , "image2new.jpg"))
 
 for eachObject in detections:
-    print(eachObject["name"] + " : " + eachObject["percentage_probability"] )
-
-detections, extracted_images = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), extract_detected_objects=True)
+print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+print("--------------------------------")
