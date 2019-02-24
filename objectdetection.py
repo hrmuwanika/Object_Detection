1f0ff1f30089049e8a7419e8cfcd8306487b1ea8
Henry Robert Muwanika <hrmuwanika@gmail.com>
2019-02-24 21:02:39 +0200

    Update objectdetection.py

--- objectdetection.py
+++ objectdetection.py
@@ -10,5 +10,5 @@ detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image2.jpg"), output_image_path=os.path.join(execution_path , "image2new.jpg"))
 
 for eachObject in detections:
-print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-print("--------------------------------")
+  print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+  print("--------------------------------")
