6573af8103d72229d5be012934fac96e42490e26
Henry Robert Muwanika <hrmuwanika@gmail.com>
2019-11-02 19:27:42 +0200

    Add files via upload

--- objectdetection.py
+++ objectdetection.py
@@ -7,7 +7,7 @@ detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image2.jpg"), output_image_path=os.path.join(execution_path , "image2new.jpg"))
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "inputimage.jpg"), output_image_path=os.path.join(execution_path , "outputimage.jpg"))
 
 for eachObject in detections:
   print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
